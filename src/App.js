d7316595ebc87edd1e4e678aba058f1e10bc1278
k2pbac <krisbachan123@hotmail.com>
2023-07-16 16:56:28 -0400

    add new search bar

--- src/App.js
+++ src/App.js
@@ -1,22 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="logo-container">
+          <div className="logo-image">
+            <img
+              src="./camera-retro-solid.svg"
+              className="App-logo"
+              alt="logo"
+            />
+          </div>
+          <div className="logo-description">
+            <h1>Hashtag Photos</h1>
+            <p>devchallenges.io</p>
+          </div>
+          <div className="input-group mb-3">
+            <img
+              className="search-icon"
+              src="./magnifying-glass-solid.svg"
+              alt="search-icon"
+            />
+            <input
+              type="text"
+              className="form-control search-input"
+              placeholder="Search by name"
+              aria-label="Example text with button addon"
+              aria-describedby="button-addon1"
+            />
+          </div>
+        </div>
       </header>
     </div>
   );
