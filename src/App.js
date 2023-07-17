d0d6d133a87401531131b03e99d1f6dd4c411e2e
k2pbac <krisbachan123@hotmail.com>
2023-07-17 07:42:20 -0400

    add form modal

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
+import { useState } from "react";
 import "./Index.css";
 import Header from "./Header";
 import ImageList from "./ImageList";
-
+import NewPhoto from "./NewPhoto";
 function App() {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   return (
     <div className="App">
       <header className="app-header">
         <Header />
-        <button className="btn btn-success btn-sm new-photo">
+        <button
+          onClick={handleShow}
+          className="btn btn-success btn-sm new-photo"
+        >
           Add a photo
         </button>
       </header>
       <ImageList />
+      <NewPhoto handleClose={handleClose} handleShow={handleShow} show={show} />
     </div>
   );
 }
---
+++ src/NewPhoto.js
@@ -0,0 +1,42 @@
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+import Form from "react-bootstrap/Form";
+function NewPhoto({ handleClose, show }) {
+  return (
+    <>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Add a new photo</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group className="mb-3" controlId="formLabel">
+              <Form.Label>Label</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="maiores repudiandae dolor"
+              />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Photo URL</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="https://unsplash.com/photos/HYtBA9xDyfg"
+              />
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Cancel
+          </Button>
+          <Button variant="success" onClick={handleClose}>
+            Submit
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+}
+
+export default NewPhoto;
