3030116b5483b57aabac007e17edcede48de9f6b
k2pbac <krisbachan123@hotmail.com>
2023-07-16 21:44:07 -0400

    finish masonry effects

---
+++ src/data.js
@@ -0,0 +1,19 @@
+const list = [
+  { url: "image1.jpg", alt: "image1", hashtag: "nature" },
+  { url: "image2.jpg", alt: "image2", hashtag: "nature" },
+  { url: "image3.jpg", alt: "image3", hashtag: "nature" },
+  { url: "image1.jpg", alt: "image1", hashtag: "nature" },
+  { url: "image2.jpg", alt: "image2", hashtag: "nature" },
+  { url: "image3.jpg", alt: "image3", hashtag: "nature" },
+  { url: "image1.jpg", alt: "image1", hashtag: "nature" },
+  { url: "image2.jpg", alt: "image2", hashtag: "nature" },
+  { url: "image3.jpg", alt: "image3", hashtag: "nature" },
+  { url: "image1.jpg", alt: "image1", hashtag: "nature" },
+  { url: "image2.jpg", alt: "image2", hashtag: "nature" },
+  { url: "image3.jpg", alt: "image3", hashtag: "nature" },
+  { url: "image1.jpg", alt: "image1", hashtag: "nature" },
+  { url: "image2.jpg", alt: "image2", hashtag: "nature" },
+  { url: "image3.jpg", alt: "image3", hashtag: "nature" },
+];
+
+export default list;
